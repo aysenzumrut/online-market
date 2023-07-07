acbf8afa417b6ea623b9ce96934b1bd54db0f6cb
aysenzumrut <aysenzumrut@windowslive.com>
2023-07-07 15:14:29 +0300

    Setting swagger with openapi

--- src/main/java/com/market/project/onlinemarket/OnlineMarketApplication.java
+++ src/main/java/com/market/project/onlinemarket/OnlineMarketApplication.java
@@ -1,9 +1,13 @@
 package com.market.project.onlinemarket;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableAutoConfiguration
+@AutoConfiguration
 @SpringBootApplication
+
 public class OnlineMarketApplication {
 
 	public static void main(String[] args) {
