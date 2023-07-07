a9374d61b244d2284c6726bc684eabb68d5127cd
aysenzumrut <aysenzumrut@windowslive.com>
2023-07-07 10:23:23 +0300

    Initial commit

---
+++ src/test/java/com/market/project/onlinemarket/OnlineMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.project.onlinemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
